9ac2cfad6c0b7193da23aabdd79db4bb68b3d221
devilzred <karthik89225@gmail.com>
2024-07-16 23:42:52 +0530

    folder structure and firebase initialzed

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:chatroom/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+  //iniltiazing Firebase 
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatRoom',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
